b3da2b1c71282ba331834fed0c6151015d221e3e
Full-Stack-Gruppe-3 <245632@usn.no>
2024-05-21 14:49:23 +0200

    DEL 1: deloppgave 2 ferdig

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Full_Stack_Gruppe_3.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Full_Stack_Gruppe_3.Controllers
+{
+    public class WeatherForecastController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Hardcode some weather forecast data
+            var forecasts = new List<WeatherForecast>
+            {
+                new WeatherForecast { Id = Guid.NewGuid(), Date = DateTime.Now, Temperature = 23.4, Comment = "Sunny" },
+                new WeatherForecast { Id = Guid.NewGuid(), Date = DateTime.Now.AddDays(1), Temperature = 19.7, Comment = "Partly cloudy" },
+                new WeatherForecast { Id = Guid.NewGuid(), Date = DateTime.Now.AddDays(2), Temperature = 21.2, Comment = "Rainy" }
+            };
+
+            return View(forecasts);
+        }
+    }
+}
---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Full_Stack_Gruppe_3.Models
+{
+    public class WeatherForecast
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public string Comment { get; set; }
+    }
+}
+
