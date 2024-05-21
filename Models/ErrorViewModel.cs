4579a2b3588863a6bdfaf8247319a03ebad63e7c
Full-Stack-Gruppe-3 <245632@usn.no>
2024-05-21 12:33:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Full_Stack_Gruppe_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
