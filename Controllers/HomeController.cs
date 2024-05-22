ba916b76eed5238f33c405361b9c55b96f6501ea
Full-Stack-Gruppe-3 <245632@usn.no>
2024-05-22 19:56:01 +0200

    DEL 2: Deloppgaev 7b(værmeldingen viser nå tempratur for siste 7 dagene, knapper er lagd)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,26 @@ namespace Full_Stack_Gruppe_3.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // For testing purposes, we'll create a mock list of observations
+            var observations = new List<Observation>
+            {
+                new Observation { ElementId = Guid.NewGuid(), Value = 15.5, Date = DateTime.Now.AddDays(-1), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 1 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 14.3, Date = DateTime.Now.AddDays(-2), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 2 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 16.7, Date = DateTime.Now.AddDays(-6), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 3 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 16.7, Date = DateTime.Now.AddDays(-10), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 3 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 16.7, Date = DateTime.Now.AddDays(-12), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 3 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 16.7, Date = DateTime.Now.AddDays(-8), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 3 },
+                new Observation { ElementId = Guid.NewGuid(), Value = 16.7, Date = DateTime.Now.AddDays(-9), TimeOffset = "UTC+1", TimeResolution = "hourly", TimeSeriesId = 3 },
+            };
+
+            var filteredObservations = FilterObservationsByLastSevenDays(observations);
+            foreach (var obs in filteredObservations)
+            {
+                Console.WriteLine($"ElementId: {obs.ElementId}, Value: {obs.Value}, Date: {obs.Date}, TimeSeriesId: {obs.TimeSeriesId}");
+            }
+
+
+            return View(filteredObservations);
         }
 
         public IActionResult Privacy()
@@ -28,5 +47,13 @@ namespace Full_Stack_Gruppe_3.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+        private static List<Observation> FilterObservationsByLastSevenDays(List<Observation> observations)
+        {
+            return observations.Where(obs => (DateTime.Now - obs.Date).TotalDays <= 7).ToList();
+        }
     }
 }
+
